80cc843d8d7bf96dbf7f558206c673c4b5c09865
rtritto <ariele.1994@gmail.com>
2024-09-05 08:49:37 +0000

    Bump vike and vike-solid; fix reactiviy of data

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -3,8 +3,8 @@ import { useData } from 'vike-solid/useData'
 import type { Data } from '@/types/myTypes'
 
 const List = () => {
-  const { nextPage } = useData<Data>()
-  return <a href={`/${nextPage}`}>Link to {nextPage}</a>
+  const data = useData<Data>()
+  return <a href={`/${data.nextPage}`}>Link to {data.nextPage}</a>
 }
 
 export default List
